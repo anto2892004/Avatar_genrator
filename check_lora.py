c9a87ebfe683569fbaa63b4c2fb5db6bab06de22
Anto Jonith A <121860967+anto2892004@users.noreply.github.com>
2025-08-14 17:03:13 +0530

    Update check_lora.py

--- check_lora.py
+++ check_lora.py
@@ -1,8 +1,9 @@
 import os
 
-lora_path = "C:/Users/tejag/Desktop/personalized_avatar/avatar_env/lora_unet/pytorch_lora_weights.bin"
+lora_path = "C:/Users/Desktop/personalized_avatar/avatar_env/lora_unet/pytorch_lora_weights.bin"
 
 if os.path.exists(lora_path):
     print("✅ LoRA weights found!")
 else:
     print("❌ LoRA weights not found. Retrain your model.")
+
